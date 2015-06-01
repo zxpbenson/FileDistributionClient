9877303face96d8835cbd15f941b0563a260c125
zhangxiaopeng <zhangxiaopeng@D-F004860.1verge.com>
2015-06-01 14:43:38 +0800

    Signed-off-by: zhangxiaopeng <zhangxiaopeng@D-F004860.1verge.com>

--- src/run.bat
+++ src/run.bat
@@ -1,3 +1,4 @@
-java -cp FileDistributionClient.jar;jsch-0.1.51.jar; com.fortappend.SwingClient true
+::java -cp FileDistributionClient.jar;jsch-0.1.51.jar; com.fortappend.SwingClient true
 ::java -Djava.ext.dirs=. com.fortappend.SwingClient true
+java -cp FileDistributionClient.jar;jsch-0.1.51.jar; com.fortappend.SwingClient true userAccount serverHost 22 ~/ authorize_list
 pause
